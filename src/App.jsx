f8c16a798ee1116b8abca8c32f086857e694e3cf
lmenil <lmenil@my.centennialcollege.ca>
2024-09-22 01:09:40 -0400

    update commit3

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <BrowserRouter basename="/portfolio">
           <Routes>
           
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
             <Route exact path="/about" element={<About />} />
             <Route exact path="/projects" element={<Projects />} />
             <Route exact path="/services" element={<Services />} />
